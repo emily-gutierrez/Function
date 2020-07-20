6ad5069f549049adf10c51c26577f59d86943bb6
emily-gutierrez <67969028+emily-gutierrez@users.noreply.github.com>
2020-07-20 11:22:27 -0500

    Add files via upload

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var a = 32
+var b = 84
+
+func plus(min: Int, max: Int) -> Int {
+    let a = 32
+    let b = 84
+    return  a + b
+}
+
+print(plus(min: a, max: b))
